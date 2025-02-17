7c19ea17b21ce816ca11d05e35dc43d6d85652fc
Christia3 <128160015+Christia3@users.noreply.github.com>
2025-02-17 13:58:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const contactSection = document.getElementById("contact");
+    
+    contactSection.addEventListener("click", () => {
+        alert("Feel free to reach out to me at yourname@example.com!");
+    });
+});
